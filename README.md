efb968ca9bc3f426007ce6012f4a7e0bf92dae27
shakil51298 <zikaiwen02@gmail.com>
2021-02-05 06:11:22 +0800

    window

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# interview
 # m-25-important
 # call apply->>object Method; objet use bind to borrow method from anotherobject;
 # shakil khan/practices/callApply impf
---
+++ window.js
@@ -0,0 +1,9 @@
+var name = "kuddus"
+function add(Number1, Number2){
+    console.log("insidekll",name);
+     sum = Number1 + Number2;
+    return sum;
+}
+console.log("name outside:",name);
+var total = add(13,12);
+console.log(sum);
