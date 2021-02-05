2f67740d4aad91ecd05e15274762179bfcafa487
shakil51298 <zikaiwen02@gmail.com>
2021-02-05 23:17:17 +0800

    set time out and SetTime Interval

---
+++ set-timeOut.js
@@ -0,0 +1,18 @@
+function doSomeThing() {
+    console.log(3333);
+}
+console.log(11111);
+// setTimeout(function(){
+//     console.log('shakil khan');
+// },1000)
+
+// setTimeout(() =>{
+//     console.log("see you latter")
+// },1000)
+// console.log(22222);
+setTimeout(doSomeThing,1000)
+console.log(1245252);
+
+// setInterval(() => {
+//     console.log('doing it again');
+// }, 1000);
